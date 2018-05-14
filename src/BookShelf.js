db32a189ba467c954d99ed160efe46a32f92989b
Euan Ramsay <euancramsay@aol.com>
2018-05-14 10:31:31 +0100

    feat: Filtering books by shelf.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -5,16 +5,15 @@ class BookShelf extends Component {
 
   render() {
     const { books } = this.props
+    console.log(books)
 
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {books.map((book) => (
-              <Book
-                key={book.id}
-                bookInfo={book}
+            {books.filter(book => book.shelf === "currentlyReading").map((book) => (
+              <Book key={book.id} bookInfo={book}
               />
             ))}
           </ol>
